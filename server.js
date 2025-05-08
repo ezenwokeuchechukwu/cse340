d39289c067f5e374cacfbbc2ada665108e6d012d
ezenwokeuchechukwu <ezenwokeuchechukwu@gmail.com>
2025-05-08 15:13:51 +0100

    completed

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
-/* ******************************************
+/******************************************
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
- *******************************************/
+ ******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
+const dotenv = require("dotenv").config()
+const path = require("path")
 const app = express()
-const static = require("./routes/static")
+
+/* ***********************
+ * Middleware
+ *************************/
+// Serve static files from the public folder
+app.use(express.static(path.join(__dirname, "public")))
 
 /* ***********************
  * View Engine and Templates
+ *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+app.set("layout", "./layouts/layout") // Points to views/layouts/layout.ejs
+
+/* ***********************
+ * Route Definitions
  *************************/
-app.use(static)
 // Index route
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
@@ -28,12 +38,12 @@ app.get("/", (req, res) => {
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500
+const host = process.env.HOST || "localhost"
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`App listening on http://${host}:${port}`)
 })
