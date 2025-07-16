1a7e393af81be9d7717902a556b578b78be74d42
ezenwokeuchechukwu <ezenwokeuchechukwu@gmail.com>
2025-07-16 19:00:17 +0100

    done

--- server.js
+++ server.js
@@ -6,10 +6,16 @@
 /* ***********************
  * Require Statements
  *************************/
+require("dotenv").config() // Load environment variables first
 const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const dotenv = require("dotenv").config()
 const path = require("path")
+const expressLayouts = require("express-ejs-layouts")
+const baseController = require("./controllers/baseController")
+const utilities = require("./utilities") // Required for getNav and handleErrors
+
+/* ***********************
+ * Create App
+ *************************/
 const app = express()
 
 /* ***********************
@@ -24,15 +30,42 @@ app.use(express.static(path.join(__dirname, "public")))
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // Points to views/layouts/layout.ejs
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"))
 
 /* ***********************
  * Route Definitions
  *************************/
-// Index route
-app.get("/", (req, res) => {
-  res.render("index", { title: "Home" });
-});
+// Index route with error handling
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+/* ***********************
+ * File Not Found Route - must be last route before the error handler
+ *************************/
+app.use(async (req, res, next) => {
+  next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
+})
+
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+
+  let message
+  if (err.status == 404) {
+    message = err.message
+  } else {
+    message = 'Oh no! There was a crash. Maybe try a different route?'
+  }
+
+  res.status(err.status || 500).render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
